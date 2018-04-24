f540bcd817218bdd68cadf7577e76128d9a93fdb
Unknown <admin@lucidlynx.net>
2018-04-24 15:31:08 -0400

    Setup + Progress Bar + JSON
    Added progress bars to the upload process
    Added support for json
    Created a setup.py

--- tool.py
+++ tool.py
@@ -1,6 +1,7 @@
 from pymarc import MARCReader
 from elasticsearch import Elasticsearch
 import click
+import json
 
 INDEX = 'library'
 TYPE = 'source'
@@ -16,16 +17,19 @@ def tool():
               help="Server of the Elastciserach instance you are attempting to connect to")
 @click.option('--port', default=9200, help='Port of the Elasticsearch instance you are attempting to connect to')
 def count(server, port):
-    client = Elasticsearch(server)
+    client = Elasticsearch(server + ':' + str(port))
 
-    count = client.count(index=INDEX, doc_type=TYPE, body={"query": {"match_all": {}}})
-    click.echo('Total Books in Libaray: %s' % count['count'])
+    book_count = client.count(index=INDEX, doc_type=TYPE, body={"query": {"match_all": {}}})
+    click.echo('Total Books in Libaray: %s' % book_count['count'])
     click.echo('Server: %s' % server)
 
 
 @tool.command(help='Sends the index data to the Elasticsearch server ')
-def send_mapping():
-    client = Elasticsearch('localhost')
+@click.option('--server', default="localhost",
+              help="Server of the Elastciserach instance you are attempting to connect to")
+@click.option('--port', default=9200, help='Port of the Elasticsearch instance you are attempting to connect to')
+def send_mapping(server, port):
+    client = Elasticsearch(server, port)
     mappings = {
         'mappings': {
             'source': {
@@ -40,8 +44,7 @@ def send_mapping():
                     "Volume": {"type": "text"},
                     "Edition": {"type": "text"},
                     "Publisher": {"type": "text"},
-                    "Year Published": {"type": "text"},
-                    # Is Date Published supposed to be something else?
+                    # Instead of having both Year and Date published, we now just have Date Published
                     "Date Published": {"type": "text"},
 
                     "Website Title": {"type": "text"},
@@ -55,39 +58,71 @@ def send_mapping():
             }
         }
     }
-    client.indices.create(index='library', body=mappings)
+    client.indices.create(index=INDEX, body=mappings)
 
 
 @tool.command(help='Sends the contents of a .marc file to the server')
+@click.argument('file_input', type=click.File('rb'))
 @click.option('--server', default="localhost",
               help="Server of the Elastciserach instance you are attempting to connect to")
 @click.option('--port', default=9200, help='Port of the Elasticsearch instance you are attempting to connect to')
-def send_marc(server, port):
-    client = Elasticsearch('localhost')
+def marc(server, port, file_input):
+    client = Elasticsearch(server + ':' + str(port))
 
-    # count is used to make sure we dont overwrite any ids in elasticsearc, x is just there to keep track of how many we have submitted.
-    count = return_source_count(server)
-    x = 0
-    with open('BooksAll.part01.marc8', 'rb') as books:
-        reader = MARCReader(books)
-        for record in reader:
+    # book_count is used to make sure we dont overwrite any ids in elasticsearc, x is just there to keep track of how many we have submitted.
+    # file_count is to get the amount of files for the progress bar
+    file_count = 0
+    book_count = return_source_count(server, port)
+
+    reader = MARCReader(file_input)
+    for record in reader:
+        file_count += 1
+    print(file_count)
+
+    file_input.seek(0)
+    reader = MARCReader(file_input)
+    with click.progressbar(reader, length=file_count) as bar:
+        for record in bar:
+            # PyMarc adds a \ to the end of every title, so here we are stripping it
+            title = record.title()
+            title = title[:-1]
+
             data = {
-                'Title': record.title(),
+                'Title': title,
                 'Author': record.author(),
-                'Year': record.pubyear(),
+                'Date Published': record.pubyear(),
                 'Publisher': record.publisher(),
                 'ISBN': record.isbn()
             }
-            test = record.as_json()
-            client.index(index='library', doc_type='source', id=count, body=data)
 
-            x += 1
-            if x % 50 == 0:
-                print("Count: " + str(x))
+            client.index(index=INDEX, doc_type=TYPE, id=book_count, body=data)
+            book_count += 1
+
+
+@tool.command(help='Sends the contents of a .json file to the server')
+@click.argument('file_input', type=click.File('rb'))
+@click.option('--server', default="localhost",
+              help="Server of the Elastciserach instance you are attempting to connect to")
+@click.option('--port', default=9200, help='Port of the Elasticsearch instance you are attempting to connect to')
+def send_json(server, port, file_input):
+    client = Elasticsearch(server + ':' + str(port))
+    book_count = return_source_count(server, port)
+
+    json_data = file_input.read()
+    json_data = json.loads(json_data)
+
+
+    length = len(json_data['sources'])
+
+    with click.progressbar(json_data['sources']) as bar:
+        for data in bar:
+            client.index(index=INDEX, doc_type='source', id=book_count, body=data)
+            book_count += 1
+
 
 
 def return_source_count(server, port):
-    client = Elasticsearch(server)
+    client = Elasticsearch(server + ':' + str(port))
 
     count = client.count(index=INDEX, doc_type=TYPE, body={"query": {"match_all": {}}})
     click.echo('Total Books in Libaray: %s' % count['count'])
